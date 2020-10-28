002947ceab4c0e1f904e2abcf11774188047aab3
Hyper <70685160+hyper-ub@users.noreply.github.com>
2020-10-28 14:47:33 +0530

    Update sample_config.py

--- tg_bot/sample_config.py
+++ tg_bot/sample_config.py
@@ -44,7 +44,7 @@ class Config(object):
     API_OPENWEATHER = False #Get API_OPENWEATHER FROM OFFICAL SITE https://da.gd/VAW3
     AI_API_KEY = None # Coffeehouse chatbot api key, get one from https://coffeehouse.intellivoid.info/
     WALL_API = None # Get one from https://wall.alphacoders.com/api.php
-
+    INFOPIC = True 
 
 class Production(Config):
     LOGGER = True
