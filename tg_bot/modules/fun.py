10bd515f34447c27786bd175569f0c4bbb4c4590
Hyper <70685160+hyper-ub@users.noreply.github.com>
2020-10-30 20:48:57 +0530

    Update fun.py

--- tg_bot/modules/fun.py
+++ tg_bot/modules/fun.py
@@ -167,6 +167,6 @@ dispatcher.add_handler(DECIDE_HANDLER)
 dispatcher.add_handler(TABLE_HANDLER)
 
 __mod_name__ = "FUN"
-__command_list__ = ["runs", "slap", "roll", "toss", "shrug", "bluetext", "rlg", "decide", "table"]
+__command_list__ = ["runs", "slap", "roll", "toss", "shrug", "bluetext", "rlg", "decide", "table", "abuse", "cutie"]
 __handlers__ = [RUNS_HANDLER, SLAP_HANDLER, ROLL_HANDLER, TOSS_HANDLER, SHRUG_HANDLER, BLUETEXT_HANDLER, RLG_HANDLER,
-                DECIDE_HANDLER, TABLE_HANDLER]
+                DECIDE_HANDLER, TABLE_HANDLER, CUTIE_HANDLER]
