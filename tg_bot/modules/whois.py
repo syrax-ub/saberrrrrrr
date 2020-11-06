5ce31db5177f1d959109118d2ddcb548f342c8fb
Hyper <70685160+hyper-ub@users.noreply.github.com>
2020-11-06 20:18:19 +0530

    Update whois.py

--- tg_bot/modules/whois.py
+++ tg_bot/modules/whois.py
@@ -109,21 +109,20 @@ def whois(bot: Bot, update: Update, args: List[str]):
         if mod_info:
             text += "\n" + mod_info
     try:
+        profile = bot.get_user_profile_photos(user.id).photos[0][-1]
+        _file = bot.get_file(profile["file_id"])
+        _file.download(f"{user.id}.png")
 
-            profile = bot.get_user_profile_photos(user.id).photos[0][-1]
-            _file = bot.get_file(profile["file_id"])
-            _file.download(f"{user.id}.png")
+         message.reply_document(
+         document=open(f"{user.id}.png", "rb"),
+         caption=(text),
+          parse_mode=ParseMode.HTML,
+          disable_web_page_preview=True)
 
-            message.reply_document(
-                document=open(f"{user.id}.png", "rb"),
-                caption=(text),
-                parse_mode=ParseMode.HTML,
-                disable_web_page_preview=True)
-
-            os.remove(f"{user.id}.png")
+    os.remove(f"{user.id}.png")
         # Incase user don't have profile pic, send normal text
-        except IndexError:
-            message.reply_text(text, parse_mode=ParseMode.HTML, disable_web_page_preview=True)
+except IndexError:
+    message.reply_text(text, parse_mode=ParseMode.HTML, disable_web_page_preview=True)
 
     else:
         message.reply_text(text, parse_mode=ParseMode.HTML, disable_web_page_preview=True)
