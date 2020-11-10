7a227b0594b4be8d282a99c263bdf9523cb204de
Hyper <70685160+hyper-ub@users.noreply.github.com>
2020-11-10 11:51:39 +0530

    Create type.py

---
+++ tg_bot/modules/type.py
@@ -0,0 +1,36 @@
+from asyncio import sleep
+from random import choice, getrandbits, randint
+from re import sub
+import time
+from collections import deque
+import requests
+from tg_bot.events import register
+from telethon import events
+
+
+
+@register(pattern=r"^/type(?: |$)(.*)", outgoing=True)
+async def typewriter(typew):
+    """ Just a small command to make your keyboard become a typewriter! """
+    textx = await typew.get_reply_message()
+    message = typew.pattern_match.group(1)
+    if message:
+        pass
+    elif textx:
+        message = textx.text
+    else:
+        await typew.edit("`Give a text to type!`")
+        return
+    sleep_time = 0.03
+    typing_symbol = "|"
+    old_text = ""
+    await typew.edit(typing_symbol)
+    await sleep(sleep_time)
+    for character in message:
+        old_text = old_text + "" + character
+        typing_text = old_text + "" + typing_symbol
+        await typew.edit(typing_text)
+        await sleep(sleep_time)
+        await typew.edit(old_text)
+        await sleep(sleep_time)
+
