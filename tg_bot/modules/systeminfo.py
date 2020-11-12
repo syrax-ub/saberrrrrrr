026d59bef863d05b3dc4ebce55275937ecc25f46
Hyper <70685160+hyper-ub@users.noreply.github.com>
2020-11-12 11:50:50 +0530

    Update systeminfo.py

--- tg_bot/modules/systeminfo.py
+++ tg_bot/modules/systeminfo.py
@@ -4,7 +4,7 @@ import platform
 
 from datetime import datetime
 from platform import python_version, uname
-from telegram import Update, Bot, Message, Chat, ParseMode
+from telegram import Update, Bot, Message, Chat, ParseMode ,InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.ext import CommandHandler, run_async , CallbackQueryHandler
 from tg_bot.modules.helper_funcs.chat_status import whitelist_plus, dev_plus
 import tg_bot.modules.helper_funcs.git_api as git
