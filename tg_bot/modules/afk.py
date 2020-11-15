6ca1e61416700bd4e25a806199303544b4be2d0d
Hyper <70685160+hyper-ub@users.noreply.github.com>
2020-11-15 11:37:49 +0530

    Update afk.py

--- tg_bot/modules/afk.py
+++ tg_bot/modules/afk.py
@@ -17,8 +17,14 @@ AFK_REPLY_GROUP = 8
 
 @run_async
 def afk(bot: Bot, update: Update):
+    user = update.effective_user
     chat = update.effective_chat  # type: Optional[Chat]
     args = update.effective_message.text.split(None, 1)
+    if not user:
+        return
+
+    if user.id in (777000, 1087968824):
+        return
     if len(args) >= 2:
         reason = args[1]
     else:
