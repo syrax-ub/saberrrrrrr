22aeaf22c694554dc95f92b1957f739e77a3e13a
Hyper <70685160+hyper-ub@users.noreply.github.com>
2020-11-20 14:00:46 +0530

    Update pyrofuns.py

--- tg_bot/modules/pyrofuns.py
+++ tg_bot/modules/pyrofuns.py
@@ -1,1 +1,172 @@
 
+
+import html
+import regex
+import aiohttp
+
+from datetime import datetime
+
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+from tg_bot import TOKEN, OWNER_ID ,SUDO_USERS, pbot
+
+DART_E_MOJI = "🎯"
+FOOTBALL_E_MOJI="⚽"
+
+
+
+@pbot.on_message(filters.command('dice'))
+async def dice(c: Client, m: Message):
+    dicen = await c.send_dice(m.chat.id, reply_to_message_id=m.message_id)
+    await dicen.reply_text(f"The dice stopped at the number {dicen.dice.value}", quote=True)
+
+@pbot.on_message(
+    filters.command("dart")
+)
+async def throw_dart(client, message):
+    """ /dart an @AnimatedDart """
+    rep_mesg_id = message.message_id
+    if message.reply_to_message:
+        rep_mesg_id = message.reply_to_message.message_id
+    await client.send_dice(
+        chat_id=message.chat.id,
+        emoji=DART_E_MOJI,
+        disable_notification=True,
+        reply_to_message_id=rep_mesg_id
+    )
+
+pbot.on_message(filters.command('type'))
+async def type(c: Client, m: Message):
+    """ Just a small command to make your keyboard become a typewriter! """
+    textx = await typew.get_reply_message()
+    message = typew.pattern_match.group(1)
+    if message:
+        pass
+    elif textx:
+        message = textx.text
+    else:
+        await typew.edit("`Give a text to type!`")
+        return
+    sleep_time = 0.03
+    typing_symbol = "|"
+    old_text = ""
+    await typew.edit(typing_symbol)
+    await sleep(sleep_time)
+    for character in message:
+        old_text = old_text + "" + character
+        typing_text = old_text + "" + typing_symbol
+        await typew.edit(typing_text)
+        await sleep(sleep_time)
+        await typew.edit(old_text)
+        await sleep(sleep_time)
+
+@pbot.on_message(
+    filters.command("football")
+)
+async def throw_football(client, message):
+    """ /football an @Animatedfootball """
+    rep_mesg_id = message.message_id
+    if message.reply_to_message:
+        rep_mesg_id = message.reply_to_message.message_id
+    await client.send_dice(
+        chat_id=message.chat.id,
+        emoji=FOOTBALL_E_MOJI,
+        disable_notification=True,
+        reply_to_message_id=rep_mesg_id
+    )
+
+
+@pbot.on_message(filters.command("dinfo") & filters.private)
+async def ids_private(c: Client, m: Message):
+    await m.reply_text("<b>Info:</b>\n\n"
+                       "<b>Name:</b> <code>{first_name} {last_name}</code>\n"
+                       "<b>Username:</b> @{username}\n"
+                       "<b>User ID:</b> <code>{user_id}</code>\n"
+                       "<b>Language:</b> {lang}\n"
+                       "<b>Chat type:</b> {chat_type}".format(
+                           first_name=m.from_user.first_name,
+                           last_name=m.from_user.last_name or "",
+                           username=m.from_user.username,
+                           user_id=m.from_user.id,
+                           lang=m.from_user.language_code,
+                           chat_type=m.chat.type
+                       ),
+                       parse_mode="HTML")
+
+
+@pbot.on_message(filters.command("dinfo") & filters.group)
+async def ids(c: Client, m: Message):
+    data = m.reply_to_message or m
+    await m.reply_text("<b>Info:</b>\n\n"
+                       "<b>Name:</b> <code>{first_name} {last_name}</code>\n"
+                       "<b>Username:</b> @{username}\n"
+                       "<b>User ID:</b> <code>{user_id}</code>\n"
+                       "<b>Datacenter:</b> {user_dc}\n"
+                       "<b>Language:</b> {lang}\n\n"
+                       "<b>Chat name:</b> <code>{chat_title}</code>\n"
+                       "<b>Chat username:</b> @{chat_username}\n"
+                       "<b>Chat ID:</b> <code>{chat_id}</code>\n"
+                       "<b>Chat type:</b> {chat_type}".format(
+                           first_name=html.escape(data.from_user.first_name),
+                           last_name=html.escape(data.from_user.last_name or ""),
+                           username=data.from_user.username,
+                           user_id=data.from_user.id,
+                           user_dc=data.from_user.dc_id,
+                           lang=data.from_user.language_code or "-",
+                           chat_title=m.chat.title,
+                           chat_username=m.chat.username,
+                           chat_id=m.chat.id,
+                           chat_type=m.chat.type
+                       ),
+                       parse_mode="HTML")
+
+
+@pbot.on_message(filters.command("ping"))
+async def ping(c: Client, m: Message):
+    first = datetime.now()
+    sent = await m.reply_text("**Pong!**")
+    second = datetime.now()
+    await sent.edit_text(f"**Pong!** `{(second - first).microseconds / 1000}`ms")
+
+
+@pbot.on_message(filters.regex(r'^s/(.+)?/(.+)?(/.+)?') & filters.reply)
+async def sed(c: Client, m: Message):
+    exp = regex.split(r'(?<![^\\]\\)/', m.text)
+    pattern = exp[1]
+    replace_with = exp[2].replace(r'\/', '/')
+    flags = exp[3] if len(exp) > 3 else ''
+
+    count = 1
+    rflags = 0
+
+    if 'g' in flags:
+        count = 0
+    if 'i' in flags and 's' in flags:
+        rflags = regex.I | regex.S
+    elif 'i' in flags:
+        rflags = regex.I
+    elif 's' in flags:
+        rflags = regex.S
+
+    text = m.reply_to_message.text or m.reply_to_message.caption
+
+    if not text:
+        return
+
+    try:
+        res = regex.sub(
+            pattern,
+            replace_with,
+            text,
+            count=count,
+            flags=rflags,
+            timeout=1)
+    except TimeoutError:
+        await m.reply_text("Oops, your regex pattern ran for too long.")
+    except regex.error as e:
+        await m.reply_text(str(e))
+    else:
+        await c.send_message(m.chat.id, f'<pre>{html.escape(res)}</pre>',
+                             reply_to_message_id=m.reply_to_message.message_id)
+
